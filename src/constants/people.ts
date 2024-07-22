1dd6b8d0cca37f874ae05cd8ca190514271e0afd
Ricky Roy Nardson <rickyroynardson@gmail.com>
2024-07-23 00:22:08 +0700

    feat(people): add peoples to section

--- src/constants/people.ts
+++ src/constants/people.ts
@@ -6,43 +6,178 @@ interface People {
 
 export const PEOPLE: People[] = [
   {
-    name: "Hernando",
-    role: "Engineer",
-    image: "/people/hernando.webp",
+    name: "Josua Yoprisyanto",
+    role: "Core",
+    image: "/people/josuayoprisyanto.webp",
   },
   {
-    name: "Ricky Roy Nardson",
-    role: "Engineer",
-    image: "/people/rickyroynardson.webp",
-  },
-  {
-    name: "Joyce",
-    role: "Relations",
-    image: "/people/joyce.webp",
-  },
-  {
-    name: "Will",
-    role: "Designer",
-    image: "/people/owil.webp",
-  },
-  {
-    name: "Hernando",
-    role: "Engineer",
-    image: "/people/hernando.webp",
-  },
-  {
-    name: "Ricky Roy Nardson",
-    role: "Engineer",
-    image: "/people/rickyroynardson.webp",
+    name: "Jeffry Arispratama",
+    role: "Core",
+    image: "/people/jeffryarispratama.webp",
   },
   {
     name: "Joyce",
-    role: "Relations",
+    role: "Core",
     image: "/people/joyce.webp",
   },
   {
-    name: "Will",
-    role: "Designer",
+    name: "Syasya",
+    role: "Core",
+    image: "/people/syasya.webp",
+  },
+  {
+    name: "Jenny",
+    role: "Core",
+    image: "/people/jenny.webp",
+  },
+  {
+    name: "Willson",
+    role: "Core",
+    image: "/people/willson.webp",
+  },
+  {
+    name: "William",
+    role: "Multimedia",
+    image: "/people/william.webp",
+  },
+  {
+    name: "Anderson",
+    role: "Programming",
+    image: "/people/anderson.webp",
+  },
+  {
+    name: "Clarita",
+    role: "Relations",
+    image: "/people/clarita.webp",
+  },
+  {
+    name: "Ricky Roy Nardson",
+    role: "Programming",
+    image: "/people/rickyroynardson.webp",
+  },
+  {
+    name: "Hernando",
+    role: "Programming",
+    image: "/people/hernando.webp",
+  },
+  {
+    name: "Owill",
+    role: "Programming",
     image: "/people/owil.webp",
+  },
+  {
+    name: "Encik Mohammad",
+    role: "Multimedia",
+    image: "/people/encik.webp",
+  },
+  {
+    name: "Jonathan",
+    role: "Multimedia",
+    image: "/people/jonathan.webp",
+  },
+  {
+    name: "Ruben",
+    role: "Multimedia",
+    image: "/people/ruben.webp",
+  },
+  {
+    name: "Kianata",
+    role: "Multimedia",
+    image: "/people/kianata.webp",
+  },
+  {
+    name: "David",
+    role: "Multimedia",
+    image: "/people/david.webp",
+  },
+  {
+    name: "Darren",
+    role: "Multimedia",
+    image: "/people/darren.webp",
+  },
+  {
+    name: "Jovan Lim",
+    role: "Multimedia",
+    image: "/people/jovanlim.webp",
+  },
+  {
+    name: "Shelomitha",
+    role: "Multimedia",
+    image: "/people/shelomitha.webp",
+  },
+  {
+    name: "Haura",
+    role: "Multimedia",
+    image: "/people/haura.webp",
+  },
+  {
+    name: "Anatasya",
+    role: "Multimedia",
+    image: "/people/anatasya.webp",
+  },
+  {
+    name: "Zaynulia",
+    role: "Multimedia",
+    image: "/people/zaynulia.webp",
+  },
+  {
+    name: "Joeann Annalise",
+    role: "Multimedia",
+    image: "/people/joeannannalise.webp",
+  },
+  {
+    name: "Cristine",
+    role: "Multimedia",
+    image: "/people/cristine.webp",
+  },
+  {
+    name: "Britney Cholas",
+    role: "Multimedia",
+    image: "/people/britneycholas.webp",
+  },
+  {
+    name: "Cendy",
+    role: "Programming",
+    image: "/people/cendy.webp",
+  },
+  {
+    name: "Arman",
+    role: "Programming",
+    image: "/people/arman.webp",
+  },
+  {
+    name: "Angelina Rianti",
+    role: "Relations",
+    image: "/people/angelinarianti.webp",
+  },
+  {
+    name: "R. Ilham",
+    role: "Relations",
+    image: "/people/rilham.webp",
+  },
+  {
+    name: "Shalsabila",
+    role: "Relations",
+    image: "/people/shalsabila.webp",
+  },
+  {
+    name: "Sherlyn",
+    role: "Relations",
+    image: "/people/sherlyn.webp",
+  },
+  {
+    name: "Eka Meriyadi",
+    role: "Relations",
+    image: "/people/eka.webp",
+  },
+  {
+    name: "Putri Sinta",
+    role: "Relations",
+    image: "/people/putri.webp",
+  },
+  {
+    name: "Sensy",
+    role: "Relations",
+    image: "/people/sensy.webp",
   },
 ];
